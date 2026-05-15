6a00322e9a43cfb410f585e7f048f43057891d98
Guardianship <960559847@qq.com>
2026-05-15 11:06:10 +0800

    fix(build): 修复 Kotlin DSL 语法错误
    - vectorDrawables 块中使用 useSupportLibrary 替代 useVectorDrawables

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
-            useVectorDrawables = true
+            useSupportLibrary = true
         }
     }
 
